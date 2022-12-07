af13c5e64b05627d1ed238c464df546cd9a2b6a9
James Best <jim@justjibba.net>
2022-12-07 05:35:35 +0000

    Set up connection settings

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -15,11 +15,22 @@ import (
 
 const version = "1.0.0"
 
+// MaxOpenConns - should be comfortably below any hard limit (often 100)
+// The higher this number is the more queries can be run but this can also
+// lead to ddos attacks. To many connections leads tp "sorry, to many clients already"
+
+// MaxIdleConns - higher value should lead to better perf but too large a pool can
+// make things worse. This should be less or equal to MaxOpenConns
+
+// MaxIdleTime - Is the time the idle conns can live for
 type config struct {
 	port int
 	env  string
 	db   struct {
-		dsn string
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
 	}
 }
 
@@ -35,6 +46,9 @@ func main() {
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment(development|staging|production)")
 	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgresSQL dsn")
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgresSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgresSQL max idle connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgresSQL max connection idle time")
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
@@ -71,6 +85,16 @@ func openDB(cfg config) (*sql.DB, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetConnMaxIdleTime(duration)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
