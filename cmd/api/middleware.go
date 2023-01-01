d6250761e7f37a68372ff7ab710dd313da73f3ce
James Best <jim@justjibba.net>
2023-01-01 05:18:29 +0000

    Add global rate limiter based on ip

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"sync"
 
 	"golang.org/x/time/rate"
 )
@@ -19,16 +21,54 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 	})
 }
 
-func (app *application) rateLimt(next http.Handler) http.Handler {
-	// initialise new rate limiter that allows average 2 requests per second
-	// with max burst of 4
-	limiter := rate.NewLimiter(2, 4)
+// This is replaces with our ip based rate limiter
+// func (app *application) rateLimt(next http.Handler) http.Handler {
+// 	// initialise new rate limiter that allows average 2 requests per second
+// 	// with max burst of 4
+// 	limiter := rate.NewLimiter(2, 4)
+//
+// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		if !limiter.Allow() {
+// 			app.rateLimitExceededResponse(w, r)
+// 			return
+// 		}
+//
+// 		next.ServeHTTP(w, r)
+// 	})
+// }
+
+func (app *application) rateLimit(next http.Handler) http.Handler {
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*rate.Limiter)
+	)
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !limiter.Allow() {
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
+		}
+
+		// Lock the mutex to prevent this code from being executed
+		// concurrently
+		mu.Lock()
+
+		if _, found := clients[ip]; !found {
+			clients[ip] = rate.NewLimiter(2, 4)
+		}
+
+		// Call the Allow() method on the rate limiter for the current IP.
+		// If its not allowed, unlock the mutex and send 429
+		if !clients[ip].Allow() {
+			mu.Unlock()
 			app.rateLimitExceededResponse(w, r)
 			return
 		}
+
+		// Very important to unluck the mutex before calling next
+		mu.Unlock()
 
 		next.ServeHTTP(w, r)
 	})
