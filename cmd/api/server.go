997f21573beee1e366bc641c1a2898b0fdf52058
James Best <jim@justjibba.net>
2023-02-02 05:27:17 +0000

    Add shutdown handling go routine

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -15,8 +18,30 @@ func (app *application) serve() error {
 		WriteTimeout: 30 * time.Second,
 	}
 
+	go func() {
+
+		// Create quit channel which carries os.Signal values
+		// Using buffered channel as signal.Notify does not wait
+		// and so we need to be ready
+		quit := make(chan os.Signal, 1)
+
+		// use signal.Notify to listen for incoming SIGINT and SIGTERM signals
+		// and relay them to the channel
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		// Read the signal from the quit channel. This code will
+		// block until the signal is recieved
+		s := <-quit
+
+		app.logger.PrintInfo("caught signal", map[string]string{"signal": s.String()})
+
+		os.Exit(0)
+	}()
+
 	app.logger.PrintInfo("starting server", map[string]string{
 		"addr": srv.Addr,
 		"env":  app.config.env,
 	})
+
+	return srv.ListenAndServe()
 }
